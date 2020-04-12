082f24389f7a937ff09dd3377998ef7368135a54
Gaurang <gaurang3101@gmail.com>
2020-04-12 17:38:58 +0530

    Code completed. Testing Commence

--- app/src/main/java/com/example/tipcalc/MainActivity.java
+++ app/src/main/java/com/example/tipcalc/MainActivity.java
@@ -10,26 +10,49 @@ import android.widget.TextView;
 import java.util.Scanner;
 
 public class MainActivity extends AppCompatActivity {
-    private int tip = 0;
-    private TextView tip10;
     Scanner scanner = new Scanner(System.in);
+    private double tip = 0;
+    private double bill = 0;
+    private double total = 0;
+    private TextView tipAmount;
+    private TextView totalAmount;
     private EditText billTotal;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        tip10 = (TextView) findViewById(R.id.tipAmountTextView);
+        tipAmount = (TextView) findViewById(R.id.tipAmountTextView);
+        totalAmount = (TextView) findViewById(R.id.totalAmountTextView);
         billTotal = (EditText) findViewById(R.id.billTotalEditText);
+        bill = Double.valueOf(billTotal.getText().toString());
     }
 
     public void calculate10(View view) {
-
+        double billInitial = bill;
+        tip = bill * 0.1;
+        total = bill + tip;
+        tipAmount.setText("Tip: "+ Double.toString(tip));
+        totalAmount.setText("Total: "+ Double.toString(total));
+        bill = billInitial;
     }
 
     public void calculate15(View view) {
+        double billInitial = bill;
+        tip = bill * 0.15;
+        total = bill + tip;
+        tipAmount.setText("Tip: "+ Double.toString(tip));
+        totalAmount.setText("Total: "+ Double.toString(total));
+        bill = billInitial;
     }
 
     public void calculate20(View view) {
+        double billInitial = bill;
+        tip = bill * 0.15;
+        total = bill + tip;
+        tipAmount.setText("Tip: "+ Double.toString(tip));
+        totalAmount.setText("Total: "+ Double.toString(total));
+        bill = billInitial;
     }
 }
