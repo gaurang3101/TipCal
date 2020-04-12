c7389f6eca481eb9ea737bb484a94a5929bb540c
Gaurang <gaurang3101@gmail.com>
2020-04-12 19:54:07 +0530

    Changes in code. Previous code had some errors. Testing resulted in a working app but the code needs to be refined

--- app/src/main/java/com/example/tipcalc/MainActivity.java
+++ app/src/main/java/com/example/tipcalc/MainActivity.java
@@ -3,14 +3,13 @@ package com.example.tipcalc;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import java.util.Scanner;
 
 public class MainActivity extends AppCompatActivity {
-    Scanner scanner = new Scanner(System.in);
     private double tip = 0;
     private double bill = 0;
     private double total = 0;
@@ -18,41 +17,58 @@ public class MainActivity extends AppCompatActivity {
     private TextView totalAmount;
     private EditText billTotal;
 
+    private double parseDouble(String s){
+        if(s == null || s.isEmpty())
+            return 0.0;
+        else
+            return Double.parseDouble(s);
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        tipAmount = (TextView) findViewById(R.id.tipAmountTextView);
-        totalAmount = (TextView) findViewById(R.id.totalAmountTextView);
-        billTotal = (EditText) findViewById(R.id.billTotalEditText);
-        bill = Double.valueOf(billTotal.getText().toString());
+        tipAmount = findViewById(R.id.tipAmountTextView);
+        totalAmount = findViewById(R.id.totalAmountTextView);
+        billTotal = findViewById(R.id.billTotalEditText);
+        //billTotal.getText().toString();
+        //bill = parseDouble(billTotal.getText().toString());
     }
 
     public void calculate10(View view) {
+        bill = parseDouble(billTotal.getText().toString());
         double billInitial = bill;
         tip = bill * 0.1;
         total = bill + tip;
-        tipAmount.setText("Tip: "+ Double.toString(tip));
-        totalAmount.setText("Total: "+ Double.toString(total));
+        String tipS = "Tip: " + tip;
+        String totalS = "Total: " + total;
+        tipAmount.setText(tipS);
+        totalAmount.setText(totalS);
         bill = billInitial;
     }
 
     public void calculate15(View view) {
+        bill = parseDouble(billTotal.getText().toString());
         double billInitial = bill;
         tip = bill * 0.15;
         total = bill + tip;
-        tipAmount.setText("Tip: "+ Double.toString(tip));
-        totalAmount.setText("Total: "+ Double.toString(total));
+        String tipS = "Tip: " + tip;
+        String totalS = "Total: " + total;
+        tipAmount.setText(tipS);
+        totalAmount.setText(totalS);
         bill = billInitial;
     }
 
     public void calculate20(View view) {
+        bill = parseDouble(billTotal.getText().toString());
         double billInitial = bill;
-        tip = bill * 0.15;
+        tip = bill * 0.20;
         total = bill + tip;
-        tipAmount.setText("Tip: "+ Double.toString(tip));
-        totalAmount.setText("Total: "+ Double.toString(total));
+        String tipS = "Tip: " + tip;
+        String totalS = "Total: " + total;
+        tipAmount.setText(tipS);
+        totalAmount.setText(totalS);
         bill = billInitial;
     }
 }
